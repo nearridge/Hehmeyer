85bd9d066d77e15832a68fb2ab381773c3ad2380
Neeraj Sharma <neerajs@uchicago.edu>
2020-09-30 13:49:42 -0500

    psuedocode for iteration to get full file.

--- data_pull.R
+++ data_pull.R
@@ -2,7 +2,12 @@ library(tidyverse)
 library(httr)
 library(curl)
 
-# Data this approach only goes to 2017
+# Data this approach only goes to May 2016
 # https://www.reddit.com/r/BitMEX/comments/6v2ns4/does_bitmex_provide_a_way_to_download_historical/
-curl("https://www.bitmex.com/api/v1/funding?_format=csv&count=100&symbol=XBT&reverse=true&startTime=2017") %>%
+
+
+# while date <= current date, pull again. Original date is static 2016-05-14 12:00:00
+# start_count <- 0, incriment by 500 
+# bind_rows into blank df 
+curl("https://www.bitmex.com/api/v1/funding?_format=csv&count=500&start=0&symbol=XBT&reverse=false") %>%
   read_csv()
