349c84ab96c6d5c97ca43466fa4684d65a6e730a
Neeraj Sharma <neerajs@uchicago.edu>
2020-10-01 21:30:51 -0500

    began EDA.

--- data_pull.R
+++ data_pull.R
@@ -13,15 +13,12 @@ last_funding <- current %>%
 
 hold_date <- ymd_hms("2016-05-14 08:00:00", tz = "US/Eastern")
 
-df <- curl("https://www.bitmex.com/api/v1/funding?_format=csv&count=500&start=0&symbol=XBT&reverse=false") %>%
-  read_csv() %>%
-  # Adjust times to be in Eastern time because that is easiest cuz it starts at 0:00:00.
-  mutate(timestamp = with_tz(timestamp, "US/Eastern"))
+df <- tibble()
 
 counter <- 0
 while (hold_date <= last_funding) {
   url <- glue("https://www.bitmex.com/api/v1/funding?_format=csv&count=500&start={counter}&symbol=XBT&reverse=false")
-  print(url)
+  # print(url)
   recent_pull <- curl(url) %>%
     read_csv() %>%
     # Adjust times to be in Eastern time because that is easiest cuz it starts at 0:00:00.
@@ -31,3 +28,6 @@ while (hold_date <= last_funding) {
   hold_date <- recent_pull %>%
     extract2(1,1)
 }
+
+# Uncomment to get updated data. Last updated at 10-01-2020 at 9:30 PM CST
+# write_csv(df, "funding_data.csv")
