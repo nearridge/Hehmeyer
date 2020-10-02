90e3e1514db555122b52d16f33e78b15916e43f4
Neeraj Sharma <neerajs@uchicago.edu>
2020-10-01 21:02:03 -0500

    Finished getting the data.

--- data_pull.R
+++ data_pull.R
@@ -1,13 +1,33 @@
 library(tidyverse)
 library(httr)
 library(curl)
+library(lubridate)
+library(magrittr)
+library(glue)
+ 
+# Get most recent funding period
+current <- now() %>% 
+  with_tz("US/Eastern") 
+last_funding <- current %>% 
+  floor_date("8 hour")
 
-# Data this approach only goes to May 2016
-# https://www.reddit.com/r/BitMEX/comments/6v2ns4/does_bitmex_provide_a_way_to_download_historical/
+hold_date <- ymd_hms("2016-05-14 08:00:00", tz = "US/Eastern")
 
+df <- curl("https://www.bitmex.com/api/v1/funding?_format=csv&count=500&start=0&symbol=XBT&reverse=false") %>%
+  read_csv() %>%
+  # Adjust times to be in Eastern time because that is easiest cuz it starts at 0:00:00.
+  mutate(timestamp = with_tz(timestamp, "US/Eastern"))
 
-# while date <= current date, pull again. Original date is static 2016-05-14 12:00:00
-# start_count <- 0, incriment by 500 
-# bind_rows into blank df 
-curl("https://www.bitmex.com/api/v1/funding?_format=csv&count=500&start=0&symbol=XBT&reverse=false") %>%
-  read_csv()
+counter <- 0
+while (hold_date <= last_funding) {
+  url <- glue("https://www.bitmex.com/api/v1/funding?_format=csv&count=500&start={counter}&symbol=XBT&reverse=false")
+  print(url)
+  recent_pull <- curl(url) %>%
+    read_csv() %>%
+    # Adjust times to be in Eastern time because that is easiest cuz it starts at 0:00:00.
+    mutate(timestamp = with_tz(timestamp, "US/Eastern"))
+  df <- bind_rows(df, recent_pull)
+   counter <- counter + 500
+  hold_date <- recent_pull %>%
+    extract2(1,1)
+}
