b264fee6f0adcc20fbe82a95df2f29a14ae9ae34
Neeraj Sharma <neerajs@uchicago.edu>
2020-11-01 17:05:10 -0600

    Updated for Ming.

--- R Work/data_pull.R
+++ R Work/data_pull.R
@@ -29,5 +29,5 @@ while (hold_date <= last_funding) {
     extract2(1,1)
 }
 
-# Uncomment to get updated data. Last updated at 10-19-2020 at 6:13 PM CST
+# Uncomment to get updated data. Last updated at 11-01-2020 at 5:02 PM CST
 # write_csv(df, "funding_data.csv")
