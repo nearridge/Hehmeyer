ec476e2795776eb8b20aaee871e4494760517a34
Neeraj Sharma <neerajs@uchicago.edu>
2020-11-03 15:26:40 -0600

    Finished price data information.

--- R Work/trade_data_pull.R
+++ R Work/trade_data_pull.R
@@ -1,3 +1,28 @@
 library(tidyverse)
 library(glue)
 library(data.table)
+library(magrittr)
+library(lubridate)
+library(stringr)
+
+startDate <- ymd("20170605")
+endDate <- today() %>% ymd()
+op <- options(digits.secs = 10)
+
+data <- tibble()
+
+while (startDate <= endDate - 1) {
+  string <- as.character(startDate) %>% str_remove_all("-")
+  read <- fread(glue("https://s3-eu-west-1.amazonaws.com/public.bitmex.com/data/trade/{string}.csv.gz")) %>%
+    as_tibble() %>%
+    filter(symbol == "XBTUSD") %>%
+    mutate(timestamp = ymd_hms(timestamp) %>% with_tz(tz = "US/Eastern")) %>%
+    group_by(hour(timestamp)) %>%
+    filter(row_number() == 1,
+           hour(timestamp) == 4 | hour(timestamp) == 12 | hour(timestamp) == 20)
+  data <- bind_rows(data, read)
+  startDate <- startDate + 1
+}
+
+
+write_csv(data, "price_data.csv")
